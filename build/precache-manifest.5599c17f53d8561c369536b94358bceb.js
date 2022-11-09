74bd1d7074dbade237c6865516cc9deac92b07d7
Garima Gupta <garrygupta2016@gmail.com>
2022-11-09 14:22:56 +0530

    data.js

---
+++ build/precache-manifest.5599c17f53d8561c369536b94358bceb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "728392353c651c9740b2",
+    "url": "/portfolio/static/css/main.e6287fb8.chunk.css"
+  },
+  {
+    "revision": "728392353c651c9740b2",
+    "url": "/portfolio/static/js/main.72839235.chunk.js"
+  },
+  {
+    "revision": "ae4700ead3b6e1632dda",
+    "url": "/portfolio/static/js/1.ae4700ea.chunk.js"
+  },
+  {
+    "revision": "d71dd58775b399d22ab0",
+    "url": "/portfolio/static/js/2.d71dd587.chunk.js"
+  },
+  {
+    "revision": "dcf67341c5f2f9eb114e",
+    "url": "/portfolio/static/js/runtime~main.dcf67341.js"
+  },
+  {
+    "revision": "86f56695cab409fbdee7bba61167423a",
+    "url": "/portfolio/static/media/About.86f56695.jpg"
+  },
+  {
+    "revision": "52423f17570ea8bc97453df553b354b6",
+    "url": "/portfolio/static/media/phone.52423f17.png"
+  },
+  {
+    "revision": "775dfe7f630b37076c29d0e16e21ffcf",
+    "url": "/portfolio/static/media/email.775dfe7f.png"
+  },
+  {
+    "revision": "a23093fbfc09d373ba3e73bfee764a7b",
+    "url": "/portfolio/static/media/address.a23093fb.png"
+  },
+  {
+    "revision": "430eddb2a871e76402c0ec83cfe9cdff",
+    "url": "/portfolio/static/media/garry1.430eddb2.png"
+  },
+  {
+    "revision": "fcbc077f95c89781be0f6c7545c04fee",
+    "url": "/portfolio/static/media/javalogo.fcbc077f.png"
+  },
+  {
+    "revision": "b25eeeab56e7e525b4a8e5e0403ea27d",
+    "url": "/portfolio/static/media/reactlogo.b25eeeab.png"
+  },
+  {
+    "revision": "60f61d54083989e46ca4b23a1c8a660d",
+    "url": "/portfolio/static/media/jslogo.60f61d54.png"
+  },
+  {
+    "revision": "bb115b2087c2c59896d219223ab71d7e",
+    "url": "/portfolio/static/media/sqllogo.bb115b20.png"
+  },
+  {
+    "revision": "bcb3306a08b8a1c94207d7b0c2113acf",
+    "url": "/portfolio/static/media/sun.bcb3306a.png"
+  },
+  {
+    "revision": "9fd3b8d8cb74a883429dd015e313c1ee",
+    "url": "/portfolio/static/media/moon.9fd3b8d8.png"
+  },
+  {
+    "revision": "cc8a78aae48587a38ddf88bcbbe28176",
+    "url": "/portfolio/index.html"
+  }
+];
